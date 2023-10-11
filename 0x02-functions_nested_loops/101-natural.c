28e89bd187c55fbf387e6480c04db097baaa89ab
Tunde <tundeogunmodede1@gmail.com>
2023-10-10 19:14:11 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,11 +9,10 @@ int main(void)
 	int sum = 0;
 	int i;
 
-	for (i = 0 ; i < 1024 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-			sum - sum + i;
+			sum = sum + i;
 	}
 	printf("%d\n", sum);
-	return (0);
 }
