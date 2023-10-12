7574a4497412e0430f550a34ca3d8d7ff0cf2333
Tunde <tundeogunmodede1@gmail.com>
2023-10-12 04:26:12 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
- * isdigit - Entry point of the function
+ * _isdigit - Entry point of the function
  * @c: start of the function
  * Return: 0
  */
 int _isdigit(int c)
 {
-	if (
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
