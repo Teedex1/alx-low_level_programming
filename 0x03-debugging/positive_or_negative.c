231e165da069518d0a983dc11237827044de8371
Tunde <tundeogunmodede1@gmail.com>
2023-10-12 17:42:22 -0700

    main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -13,6 +13,12 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if ( i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n",i);
+
 	return (0);
 }
