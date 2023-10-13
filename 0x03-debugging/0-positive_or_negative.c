ffecf9ad4355206baeb6b29249f29602cc07864b
Tunde <tundeogunmodede1@gmail.com>
2023-10-12 17:29:38 -0700

    main.c

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+nclude <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * psotive_or_negative -
+ * @n: function
+ * Return: void
+ */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/* your code goes there */
+					return (0);
+}
