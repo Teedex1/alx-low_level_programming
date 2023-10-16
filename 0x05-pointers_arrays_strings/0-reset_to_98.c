515ddb0c4534a18d3a49c94efecb4142f4661051
Tunde <tundeogunmodede1@gmail.com>
2023-10-16 14:28:10 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - main function
- * reset_to_98: modify the value
- * @n: pointer to the value
+ * reset_to_98 - modify the value
+ *
+ * @n: pointer to takes in to modify
+ *
  * Return: Void
  */
 
 void reset_to_98(int *n)
 
-{ 
+{
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 void swap_int(int *a, int *b);
-reset_to_98(&n);
 void reset_to_98(int *n);
 
 #endif
