6ff199d93794341f3e19bc84090fd911e97129a8
Tunde <tundeogunmodede1@gmail.com>
2023-10-16 17:33:31 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - to count the strings
+ *@s: the char
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	return (count);
+}
