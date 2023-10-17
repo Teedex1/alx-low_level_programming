f5a2395c981bb960fa1129563b594d17107d1bf5
Tunde <tundeogunmodede1@gmail.com>
2023-10-17 04:25:56 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include"main.h"
 #include<stdio.h>
+
 /**
-*main - mainfunction
-*reset_to_98: modify the value
-*@n:pointertothevalue
-*Return:Void
+* reset_to_98 - modify the value
+* @n: pointertothevalue
+* Return: Void
 */
 
-void reset_to_98(int*n)
+void reset_to_98(int *n)
 
 {
 	*n = 98;
