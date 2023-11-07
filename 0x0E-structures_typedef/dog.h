ef8f56904c1acde0b8443cd39e82fd4e9cddda49
Tunde <tundeogunmodede1@gmail.com>
2023-11-07 05:29:28 -0800

    main.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
-#ifndef DOG_H
+#ifdef DOG_H
 #define DOG_H
+
 /**
  * struct dog - dog structure
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
  */
 struct dog
 {
@@ -12,5 +13,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 #endif
