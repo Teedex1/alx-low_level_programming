d9fb0b38ebf48bd8de14f562261b3cd38e60819f
Tunde <tundeogunmodede1@gmail.com>
2023-11-16 17:02:44 -0800

    linked

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,11 +11,11 @@
  * @next: pointer to next node
  * Description: singly linked list
  */
-typedef struct list_t
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_t *next;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
