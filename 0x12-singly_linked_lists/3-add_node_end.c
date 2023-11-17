03a343cafb5d94c275f2159010502ebb2272042c
Tunde <tundeogunmodede1@gmail.com>
2023-11-16 16:40:14 -0800

    linked

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * add_node - add a new node
+ * @head: pointer
+ * @str: string
+ * Return: the address or NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *up;
+	int length;
+	list_t *i, *l;
+
+	i = malloc(sizeof(list_t));
+	if (i == NULL)
+		return (NULL);
+
+	up = strdup(str);
+	if (str == NULL)
+	{
+		free(i);
+		return (NULL);
+	}
+	for (length = 0; str[length];)
+		length++;
+
+	i->str = up;
+	i->len = length;
+	i->next = *head;
+
+	if (*head == NULL)
+		*head = i;
+	else
+	{
+		l = *head;
+		while (l->next != NULL)
+			l = l->next;
+		l-> next = i;
+	}
+
+	return (*head);
+}
