da0555adbf02cdc74c1df64623c998c7afa1b68d
Tunde <tundeogunmodede1@gmail.com>
2023-11-23 18:37:35 -0800

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,13 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b == Null)
+	if (b == NULL)
 		return (0);
 
-	for (; b; ++b)
+	for (; *b != '\0'; ++b)
 	{
 		if (*b != '0' && *b != '1')
-			return (0);
+			continue;
 
 		result = (result << 1) + (*b - '0');
 	}
