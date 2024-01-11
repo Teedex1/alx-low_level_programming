3a64d7350aaf69317810271d789aa697efbb2298
Tunde <tundeogunmodede1@gmail.com>
2024-01-11 09:03:20 -0800

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - print list
+ * @h: the pointer
+ * Return: nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t node_count = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		node_count++;
+	}
+	return (node_count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Th is 0x17-doubly_linked_lists task
